883e14de1bd224b7174a31c7d4e0181a40d1c334
amh21-Ha <amha.ha07@gmail.com>
2025-01-05 23:23:23 +0300

    user repository interface

---
+++ backEnd/user-service/src/domain/user.repository.interface.js
@@ -0,0 +1,16 @@
+class UserRepositoryInterface {
+    create(user) {
+      throw new Error('Method not implemented.');
+    }
+  
+    findByEmail(email) {
+      throw new Error('Method not implemented.');
+    }
+  
+    findById(id) {
+      throw new Error('Method not implemented.');
+    }
+  }
+  
+  module.exports = UserRepositoryInterface;
+  
