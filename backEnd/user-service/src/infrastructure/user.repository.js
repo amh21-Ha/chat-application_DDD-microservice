a0bdf56787cc7470d2b4509156bebae56434986d
amh21-Ha <amha.ha07@gmail.com>
2025-01-05 23:58:18 +0300

    user repository

---
+++ backEnd/user-service/src/infrastructure/user.repository.js
@@ -0,0 +1,23 @@
+const db = require('./database');
+const UserRepositoryInterface = require('../domain/user.repository.interface');
+
+class UserRepository extends UserRepositoryInterface {
+  async create(user) {
+    const query = `INSERT INTO users (id, username, email, password_hash) VALUES ($1, $2, $3, $4)`;
+    await db.query(query, [user.id, user.username, user.email, user.passwordHash]);
+  }
+
+  async findByEmail(email) {
+    const query = `SELECT * FROM users WHERE email = $1`;
+    const result = await db.query(query, [email]);
+    return result.rows[0];
+  }
+
+  async findById(id) {
+    const query = `SELECT * FROM users WHERE id = $1`;
+    const result = await db.query(query, [id]);
+    return result.rows[0];
+  }
+}
+
+module.exports = UserRepository;
