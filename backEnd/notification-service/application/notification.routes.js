88b130c3bb5a8e8fa1a29b11dc8c414517f07c2f
amh21-Ha <amha.ha07@gmail.com>
2025-01-21 10:17:21 +0300

    routing notification

---
+++ backEnd/notification-service/application/notification.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const NotificationController = require('../notification.controller');
+const NotificationRepository = require('../../domain/notification.repository');
+const NotificationService = require('../../domain/notification.service');
+const { NotificationModel } = require('../../infrastructuredb');
+
+const router = express.Router();
+const notificationRepository = new NotificationRepository(NotificationModel);
+const notificationService = new NotificationService(notificationRepository);
+const notificationController = new NotificationController(notificationService);
+
+router.post('/', (req, res) => notificationController.sendNotification(req, res));
+router.get('/:userId', (req, res) => notificationController.getNotifications(req, res));
+router.patch('/:notificationId', (req, res) => notificationController.markNotificationAsRead(req, res));
+
+module.exports = router;
