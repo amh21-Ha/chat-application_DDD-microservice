099472f638e74c5d7b611ea097d5314df8503b1f
amh21-Ha <amha.ha07@gmail.com>
2025-01-21 11:29:13 +0300

    group repository

---
+++ backEnd/group-service/domain/group.repository.js
@@ -0,0 +1,15 @@
+class GroupRepository {
+    async createGroup(groupData) {
+        throw new Error('Method not implemented');
+    }
+    async addUserToGroup(groupId, userId) {
+        throw new Error('Method not implemented');
+    }
+    async removeUserFromGroup(groupId, userId) {
+        throw new Error('Method not implemented');
+    }
+    async getGroupDetails(groupId) {
+        throw new Error('Method not implemented');
+    }
+}
+module.exports = GroupRepository;
