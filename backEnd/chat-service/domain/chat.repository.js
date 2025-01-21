95f4aa2c571c3c9dd6d7b2b4b92593cf701d699a
amh21-Ha <amha.ha07@gmail.com>
2025-01-21 11:58:25 +0300

    chat repository that needs expand

---
+++ backEnd/chat-service/domain/chat.repository.js
@@ -0,0 +1,9 @@
+class ChatRepository {
+    async saveMessage(message) {
+        throw new Error('Method not implemented');
+    }
+    async getMessagesBetweenUsers(userId1, userId2) {
+        throw new Error('Method not implemented');
+    }
+}
+module.exports = ChatRepository;
