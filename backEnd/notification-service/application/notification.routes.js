b0928d2bed6f4f5d5ba960b4f6005b857cc8b99b
amh21-Ha <amha.ha07@gmail.com>
2025-01-21 15:47:08 +0300

    db

--- backEnd/notification-service/application/notification.routes.js
+++ backEnd/notification-service/application/notification.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const NotificationController = require('../notification.controller');
 const NotificationRepository = require('../../domain/notification.repository');
 const NotificationService = require('../../domain/notification.service');
-const { NotificationModel } = require('../../infrastructure.db');
+const { NotificationModel } = require('../../infrastructure/db');
 
 const router = express.Router();
 const notificationRepository = new NotificationRepository(NotificationModel);
